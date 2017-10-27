5ea3a99ab036e28629888d36d863d082143d8964
Alexandr Treboganov <alex@quokka.pro>
2017-10-27 17:55:46 +0300

    22

--- app/js/common.js
+++ app/js/common.js
@@ -93,6 +93,29 @@ $(function() {
 		}
 	});
 
+	$('.partnerss').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		nav: true,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			800: {
+				items: 3
+			},
+			1100: {
+				items: 4
+			}
+		}
+	});
+
 	$('.workers').owlCarousel({
 		loop: true,
 		smartSpeed: 700,
